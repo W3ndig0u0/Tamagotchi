1812d31b70f7dd378edd44e3a0a09724b6caf344
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-09-02 10:09:52 +0200

    a

---
+++ Tamogotchi/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Tamogotchi
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
